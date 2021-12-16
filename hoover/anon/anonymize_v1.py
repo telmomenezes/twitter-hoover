9013158d520bd1dff4f0b43de122bd65743f8c85
Manuel <manuel.tonneau@mailbox.org>
2021-12-16 15:05:59 +0100

    switch from debug to info

--- hoover/anon/anonymize_v1.py
+++ hoover/anon/anonymize_v1.py
@@ -25,7 +25,7 @@ import shutil
 #                             filemode='a',
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
